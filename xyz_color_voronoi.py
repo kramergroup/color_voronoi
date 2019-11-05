20af078887b0b29c132f708a95fe19fede1eaff0
Andrea Silva <andreasilva759@gmail.com>
2019-11-05 00:54:43 +0000

    Polygon alpha+noedge, dpi option, correct savefig bug
    Details:
    - Add polygon alpha option and set edge width to 0, as they are already
    plotted by vornoi_plot_2d.
    - Add set dpi option in standalone script
    - Correct order of plt.show and fig.savefig: now first save then show,
    so not reset and blank save.

--- xyz_color_voronoi.py
+++ xyz_color_voronoi.py
@@ -2,7 +2,7 @@
 
 """Module description here
 
-Silva 01-11-2019"""
+Silva 05-11-2019"""
 
 import sys, argparse, logging, json
 import numpy as np
@@ -21,45 +21,49 @@ def get_y(l):
     """Get y coordinate from list of positions"""
     return [x[1] for x in l]
 
-def get_xyz_col(xyz_stream, n): 
-     """Return the nth column of an xyz stream""" 
+def get_xyz_col(xyz_stream, n):
+     """Return the nth column of an xyz stream"""
      return [l.split()[n] for i, l in enumerate(xyz_stream) if i>1 ]
 
-def color_voronoi(vor, z, ax, 
+def color_voronoi(vor, z, ax,
                   minima=None, maxima=None, c_code="RdYlBu_r",
-                  p_size=1, show_points=True, show_vertices=False,
-                  l_width=1, l_color="red", l_alpha=1):
+                  p_size=1,
+                  show_points=True, show_vertices=False,
+                  l_width=1, l_color="red", l_alpha=1,
+                  pol_alpha=1.):
     """Wrapper around scipy voronoi plot for the lines, then color the regions according to ginven z.
     Most options can be controlled from this wrapper."""
-    
+
     # If not fiven, find min/max values for normalization
     if minima is None: minima = min(z)
     if maxima is None: maxima = max(z)
-    
+
     # Normalize chosen colormap
     norm = mpl.colors.Normalize(vmin=minima, vmax=maxima, clip=True)
     mapper = cm.ScalarMappable(norm=norm, cmap=c_code )
-    
+
     # Plot Voronoi diagram: lines between regions and, possible, vertex
     voronoi_plot_2d(vor, ax=ax,
-                    show_points=False, show_vertices=show_vertices, 
+                    show_points=False, show_vertices=show_vertices,
                     line_alpha=l_alpha, line_width=l_width, line_color=l_color,
-                    s=0.2)    
+                    s=0.2)
     # For region index of ith point
     for i, p_r in enumerate(vor.point_region):
         # Get corresponding region
         region = vor.regions[p_r]
-        # Open regions (contain -1 as vertex) do not define a polygon 
+        # Open regions (contain -1 as vertex) do not define a polygon
         if not -1 in region:
             # Define polygon from closed voronoi region
             polygon = [vor.vertices[v] for v in region]
             # Get ith color in z list corresponding to current point
-            ax.fill(*zip(*polygon), color=mapper.to_rgba(z[i]))
-            
+            ax.fill(*zip(*polygon),
+                    alpha=pol_alpha, linewidth=0, # No edges, voronoi_plot_2d takes care of that
+                    color=mapper.to_rgba(z[i]))
+
     # If required, plot the points generating the Voronoi diagram
     if show_points:
         ax.plot(get_x(vor.points), get_y(vor.points), 'o', c='black', ms=p_size )
-    return ax    
+    return ax
 
 def xyz_color_vornoi(argv):
     """Compute Voronoi construction of xy posisions from xyz file and color polygons according to additional 4th column in xyz file.
@@ -68,17 +72,16 @@ def xyz_color_vornoi(argv):
     - minima=None (compute from data),
     - maxima=None (compute from data),
     - c_code='RdYlBu_r' (matplotlib palette name),
-    - p_size=0.5,
+    - p_size=0.5 (point size),
     - show_points=True (write 0/1 in json, not string),
     - show_vertices=False (0/1),
-    - l_width=1,
-    - l_color='red',
-    - l_alpha=1
+    - l_width=1 (diagram line width),
+    - l_color='red' (diagram lince color),
+    - l_alpha=1 (diagram line alpha, 0 to hide construction)
+    - pol_alpha=1 (polygon color alpha)
     """
 
-    # Output file details
-    dpi = 800
-    out_format = "png"
+
     #-------------------------------------------------------------------------------
     # Argument parser
     #-------------------------------------------------------------------------------
@@ -99,8 +102,12 @@ def xyz_color_vornoi(argv):
                         help='no iteractive plot;')
     parser.add_argument('-o', '--out',
                         dest='out_f',
-                        type=str, default=None, metavar="basename",
-                        help='save figure file in %s at %idpi'% (out_format, dpi))
+                        type=str, default=None, metavar="basename.ext",
+                        help='save figure to file. Estension inferred as last bit after last ".";')
+    parser.add_argument('--dpi',
+                        dest='dpi',
+                        type=int, default=200,
+                        help='set fiugre dpi')
     parser.add_argument('--debug',
                         action='store_true', dest='debug',
                         help='show debug information.')
@@ -118,6 +125,14 @@ def xyz_color_vornoi(argv):
         c_log.setLevel(logging.DEBUG)
         debug_opt = ['-d']
     c_log.debug(args)
+
+    # Output file details
+    dpi = args.dpi
+    # Infer estension from filename: split according to dots and take last part
+    #if args.out_f is not None:
+    #    out_format = args.out_f.split(".")[-1]
+    #    c_log.debug("Output format set to %s", out_format)
+    # Matplotlib is perfectly capable of understanding it on its own
 
     #-------------------------------------------------------------------------------
     # Read data
@@ -137,15 +152,15 @@ def xyz_color_vornoi(argv):
     # Vornoi construction
     #-------------------------------------------------------------------------------
     c_log.info("Computing Voronoi via scipy-qhull...")
-    # Get Voronoi construction 
+    # Get Voronoi construction
     vor = Voronoi(xy)
 
     #-------------------------------------------------------------------------------
     # Define canvas
     #-------------------------------------------------------------------------------
-    fig = plt.figure()   
+    fig = plt.figure()
     fig.set_dpi(dpi)
-    ax = fig.add_subplot(111)              
+    ax = fig.add_subplot(111)
     ax.set_aspect("equal")
 
     if args.plt_opt != {}:
@@ -155,13 +170,14 @@ def xyz_color_vornoi(argv):
     c_log.info("Plotting at %i dpi", dpi)
     color_voronoi(vor, vor_col, ax, **args.plt_opt)
 
-    # Show interactive plot
+    # First, save figure
+    if args.out_f != None:
+        c_log.info("Saving file on %s", args.out_f)
+        plt.savefig(args.out_f, dpi=dpi)
+    # Then, show interactive plot
     if args.plot_flg:
         plt.show()
-    # Save figure
-    if args.out_f != None:
-        c_log.info("Saving file on %s.%s", args.out_f, out_format)
-        plt.savefig("%s.%s" % (args.out_f, out_format), dpi=dpi)
+
 
     return 0
 
